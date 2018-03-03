cc113fddba57bef94d665b3ea842ce74efb055f2
orlandoperdana <orlandoperdana@gmail.com>
2018-03-03 14:27:35 +0700

    update server.js

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ var Bear     = require('./app/models/bear');
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+
+
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
